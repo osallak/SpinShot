9dced9b934485094ab60d4a9ba38271b9bbb4a0c
Navoos <iacopocoram@gmail.com>
2023-07-20 11:28:19 +0100

    structural change

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    validationSchema: Joi.object({
+      NODE_ENV: Joi.string()
+        .valid("development", "production", "test")
+        .default("development"),
+      PORT: Joi.number()
+    })
+  })],
   controllers: [],
   providers: [],
 })

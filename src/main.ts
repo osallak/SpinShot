09d353c0fd9459e7e54fc1cab375dfbd7c68475b
Oussama Sallak <osallak@student.1337.ma>
2023-07-28 09:50:18 +0100

    `chor:` enable logging (all levels)

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log', 'debug', 'verbose'],
+  });
   console.log(process.env.PORT);
   await app.listen(process.env.PORT);
 }
 bootstrap();
+
+//? if you want to use the logger, just import LoggerService
+//? from @nestjs/common and use it in your class

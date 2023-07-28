bf57dde0e1be303f6112b7320923ce2a5083aa26
ataji <tajiayoub35@gmail.com>
2023-07-28 14:21:18 +0100

    change the border color when theire is a error

--- src/Components/ui/Inputs/InputBorder.tsx
+++ src/Components/ui/Inputs/InputBorder.tsx
@@ -35,27 +35,18 @@ const InputBorder: React.FC<InputsProps> = ({
   return (
     <div
       className={`rounded-2xl w-full h-full flex flex-row items-center bg-${Color} border-opacity-40 px-2 border-${BorderSize}`}
-      style={{ borderColor: isReg ? Border : "red" }}
+      style={{ borderColor: isReg ? Border : "#FF000060" }}
     >
       <div className="w-[100%] h-full flex">
         <div className="inset-y-0 left-1.5 flex items-center pointer-events-none space-x-64">
-          {isReg ? (
             <Image src={icon} alt="username or Email" className="h-5 w-5" />
-          ) : (
-            <Image src={redicon} alt="username or Email" className="h-5 w-5" />
-          )}
         </div>
-
         <input
           type={type == "password" && ShowPassword ? "text" : type}
           onChange={handleInputChange}
           onBlur={handleBlur}
           placeholder={PlaceHolder}
-          className={`w-full bg-transparent pl-3 h-full focus:outline-none ${
-            isReg
-              ? "placeholder:text-pearl"
-              : "placeholder:text-red-900 placeholder:opacity-100"
-          } placeholder:opacity-40 placeholder:font-Poppins, font-thin text-pearl focus:placeholder:opacity-0`}
+          className={`w-full bg-transparent pl-3 h-full focus:outline-none placeholder:opacity-40 placeholder:font-Poppins, font-thin text-pearl focus:placeholder:opacity-0`}
         />
       </div>
       {type === "password" ? (

1167aad3dcd939686bb0ab1e048865ae522db9a6
Yaakoub Khoudrouf <yakhoudr@e3r10p14.1337.ma>
2023-09-19 15:36:36 +0100

    addedd the api specs for swagger

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -1,8 +1,11 @@
-import { Controller, Get, Param, Query, Res } from '@nestjs/common';
+import { Controller, Get, Param, Query, Res, UseGuards } from '@nestjs/common';
 import { Response as ExpressResponse } from 'express';
 import { ChatService } from './chat.service';
 import { PaginationQueryDto } from 'src/global/dto/pagination-query.dto';
+import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/guards';
 
+@ApiTags('chat')
 @Controller('chat')
 export class ChatController {
   // this is for bigInt because it cannot be serialized
@@ -17,6 +20,21 @@ export class ChatController {
 
   constructor(private readonly chatService: ChatService) {}
 
+  @ApiBearerAuth()
+  @ApiResponse({
+    status: 200,
+    description: 'For each conversation of a User X get it latest message',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'User was not found',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Failed to retrieve messages from database',
+  })
+	@ApiTags('chat')
+  @UseGuards(JwtAuthGuard)
   @Get('all/:id')
   async getAll(
     @Param('id') param: string,
@@ -27,6 +45,21 @@ export class ChatController {
     expressResponse.status(status).send(this.toObject.call(content));
   }
 
+  @ApiResponse({
+    status: 200,
+    description: 'Return all messages between the logged in user and a user',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Either the logged in user does not exist or the other user does not exist',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Failed to retrieve messages from database',
+  })
+	@ApiTags()
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
   @Get('individual/:id')
   async getIndividualMessagesWithAUser(
     @Param('id') userId: string,
